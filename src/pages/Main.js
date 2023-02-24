707edfd8ac75b2f7eefacda38cb12f2d5d5cf525
Laura <lauratelqiu@gmail.com>
2023-02-24 16:46:14 -0500

    tours setup

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import About from './About'
 import Features from './Features'
+import Tours from './Tours'
 
 function Main() {
   return (
     <div>
       <About />
       <Features />
+      <Tours />
     </div>
   )
 }

70f40add5a8065142b6b01b28fd6400982b011a6
Laura <lauratelqiu@gmail.com>
2023-02-23 20:35:48 -0500

    features styling

--- src/data.js
+++ src/data.js
@@ -1,53 +1,35 @@
-
-
-
 export const pageLinks = [
-  { id: 1, href: '#header', text: 'home' },
-  { id: 2, href: '#about', text: 'about us' },
-  { id: 3, href: '#rooms', text: 'rooms' },
-  { id: 4, href: '#reviews', text: 'reviews' },
-  { id: 5, href: '#footer', text: 'contact' },
-
+  { id: 1, href: "#header", text: "home" },
+  { id: 2, href: "#about", text: "about us" },
+  { id: 3, href: "#rooms", text: "rooms" },
+  { id: 4, href: "#reviews", text: "reviews" },
+  { id: 5, href: "#footer", text: "contact" },
 ];
-
 
 export const features = [
   {
     id: 1,
     icon: "feature-box__icon icon-basic-world",
-    title: 'Explore the world',
-    text: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.',
-
+    title: "Explore the world",
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.",
   },
   {
     id: 2,
-    icon1: "icon-basic-arrow-left",
-    icon: "feature-box__icon icon-basic-compass",
-    title: 'Meet nature',
-    text: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.',
 
+    icon: "feature-box__icon icon-basic-compass",
+    title: "Meet nature",
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.",
   },
   {
     id: 3,
     icon: "feature-box__icon icon-basic-map",
-    title: 'Find your way',
-    text: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.',
-
+    title: "Find your way",
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.",
   },
   {
     id: 4,
     icon: "feature-box__icon icon-basic-heart",
-    title: 'Live a healthier life',
-    text: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.',
-
-  }
-
-
-
-
+    title: "Live a healthier life",
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.",
+  },
 ];
-
-
-
-
-

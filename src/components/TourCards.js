5a89a5ac99c1a6ffcbf6c4b9fa51e42e14786bad
Laura <lauratelqiu@gmail.com>
2023-02-27 11:16:51 -0500

    front card styling

--- src/components/TourCards.js
+++ src/components/TourCards.js
@@ -7,7 +7,7 @@ function TourCards() {
     <div className="row">
 
       {tours.map((tour) => {
-        const { id, cardPicture, heading, details, cardSideBack } = tour;
+        const { id, cardPicture, heading, span, length, capacity, guides, sleep, diffculty, cardSideBack } = tour;
         return (
           <div key={id} className="col-1-of-3">
             <div className="card">
@@ -15,8 +15,20 @@ function TourCards() {
                 <div className={`card__picture ${cardPicture}`}>
                   &nbsp;
                 </div>
-                <div className="card_heading">{heading} </div>
-                <div className="card_details">{details}</div>
+                <h4 className="card__heading">
+                  <span className={`card__heading-span ${span}`}
+                  >{heading}</span
+                  >
+                </h4>
+                <div className="card__details">
+                  <ul>
+                    <li>{length}</li>
+                    <li>{capacity}</li>
+                    <li>{guides}</li>
+                    <li>{sleep}</li>
+                    <li>{diffculty}</li>
+                  </ul>
+                </div>
 
               </div>
               <div className={`card__side card__side--back ${cardSideBack}`}>BACK</div>

6369d6c474a335ad7828c198d2d86cff6d079a77
Laura <lauratelqiu@gmail.com>
2023-03-03 09:14:44 -0500

    form

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Form from '../sass/components/Form'
+
 
 function Booking() {
   return (
     <section className='section-book'>
       <div className='row'>
         <div className='book'>
+          <Form />
 
         </div>
       </div>

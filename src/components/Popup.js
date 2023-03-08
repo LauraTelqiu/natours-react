b0416a8a5beec72f06d7aab8b856e63a1446ada3
Laura <lauratelqiu@gmail.com>
2023-03-08 13:09:30 -0500

    popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,9 +1,42 @@
 import React from "react";
+import nat8 from '../img/nat-8.jpg'
+import nat9 from '../img/nat-8.jpg'
 
 function Popup() {
   return (
-    <div class="popup" id="popup">
-      <div class="popup__content">POPUP</div>
+    <div className="popup" id="popup">
+      <div className="popup__content">
+        <div className="popup__left">
+
+          <img src={nat8} alt="Tour" className="popup__img" />
+          <img src={nat9} alt="Tour" className="popup__img" />
+        </div>
+        <div className="popup__right">
+          <h2 className="heading-secondary u-margin-bottom-small">
+            Book now
+          </h2>
+          <h3 className="heading-tertiary u-margin-bottom-small">
+            Important &ndash;  Read the terms and conditions before booking.
+          </h3>
+          <p className="popup__text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed sed
+            risus pretium quam. Aliquam sem et tortor consequat id. Volutpat
+            odio facilisis mauris sit amet massa vitae. Mi bibendum neque
+            egestas congue. Placerat orci nulla pellentesque dignissim enim sit.
+            Vitae semper quis lectus nulla at volutpat diam ut venenatis.
+            Malesuada pellentesque elit eget gravida cum sociis natoque
+            penatibus et. Proin fermentum leo vel orci porta non pulvinar neque
+            laoreet. Gravida neque convallis a cras semper. Molestie at
+            elementum eu facilisis sed odio morbi quis. Faucibus vitae aliquet
+            nec ullamcorper sit amet risus nullam eget. Nam libero justo laoreet
+            sit. Amet massa vitae tortor condimentum lacinia quis vel eros
+            donec. Sit amet facilisis magna etiam. Imperdiet sed euismod nisi
+            porta.
+          </p>
+          <a href="#booking" className="btn btn--green">Book now</a>
+        </div>
+      </div>
     </div>
   );
 }

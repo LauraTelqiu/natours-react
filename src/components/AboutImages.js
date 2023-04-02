35f4c90102254303436ed621532943fa32cd182d
Laura <lauratelqiu@gmail.com>
2023-04-01 23:56:06 -0400

    responisve about images

--- src/components/AboutImages.js
+++ src/components/AboutImages.js
@@ -1,22 +1,48 @@
 import React from 'react';
-import nat1 from '../img/nat-1-large.jpg'
-import nat2 from '../img/nat-2-large.jpg'
-import nat3 from '../img/nat-3-large.jpg'
+// import nat1 from '../img/nat-1-large.jpg'
+// import nat2 from '../img/nat-2-large.jpg'
+// import nat3 from '../img/nat-3-large.jpg'
 import '../sass/main.css'
 
 
 function AboutImages() {
   return (
+
     <div className="col-1-of-2">
       <div className="composition">
+        <img srcSet="img/nat-1.jpg 300w, img/nat-1-large.jpg 1000w"
+          sizes="(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px"
+          alt="Nat-1"
+          className="composition__photo composition__photo--p1"
+          src="img/nat-1-large.jpg" />
 
-        <img src={nat1} alt="image1" className="composition__photo composition__photo--p1" />
-        <img src={nat2} alt="image2" className="composition__photo composition__photo--p2" />
-        <img src={nat3} alt="image3" className="composition__photo composition__photo--p3" />
+        <img srcSet="img/nat-2.jpg 300w, img/nat-2-large.jpg 1000w"
+          sizes="(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px"
+          alt="Nat-2"
+          className="composition__photo composition__photo--p2"
+          src="img/nat-2-large.jpg" />
+
+        <img srcSet="img/nat-3.jpg 300w, img/nat-3-large.jpg 1000w"
+          sizes="(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px"
+          alt="Nat-3"
+          className="composition__photo composition__photo--p3"
+          src="img/nat-3-large.jpg" />
       </div>
-
-
     </div>
+
+
+
+
+    // <div className="col-1-of-2">
+    //   <div className="composition">
+
+    //     <img src={nat1} alt="image1" className="composition__photo composition__photo--p1" />
+    //     <img src={nat2} alt="image2" className="composition__photo composition__photo--p2" />
+    //     <img src={nat3} alt="image3" className="composition__photo composition__photo--p3" />
+    //   </div>
+
+
+    // </div>
   )
 }
 

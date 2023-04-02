e6d892ad6f81bc41c7ed4a31e147305e71a1c3e0
Laura <lauratelqiu@gmail.com>
2023-04-02 00:32:18 -0400

    about images

--- src/components/AboutImages.js
+++ src/components/AboutImages.js
@@ -1,8 +1,12 @@
-import React from 'react';
-// import nat1 from '../img/nat-1-large.jpg'
-// import nat2 from '../img/nat-2-large.jpg'
-// import nat3 from '../img/nat-3-large.jpg'
 import '../sass/main.css'
+import React from 'react';
+import nat1 from '../img/nat-1.jpg'
+import natLarge1 from '../img/nat-1-large.jpg'
+import nat2 from "../img/nat-2.jpg"
+import natLarge2 from '../img/nat-2-large.jpg'
+import nat3 from '../img/nat-3.jpg'
+import natLarge3 from '../img/nat-3-large.jpg'
+
 
 
 function AboutImages() {
@@ -10,19 +14,20 @@ function AboutImages() {
 
     <div className="col-1-of-2">
       <div className="composition">
-        <img srcSet="img/nat-1.jpg 300w, img/nat-1-large.jpg 1000w"
+        <img srcSet={`${nat1} 300w, ${natLarge1} 1000w`}
+
           sizes="(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px"
           alt="Nat-1"
           className="composition__photo composition__photo--p1"
           src="img/nat-1-large.jpg" />
 
-        <img srcSet="img/nat-2.jpg 300w, img/nat-2-large.jpg 1000w"
+        <img srcSet={`${nat2} 300w, ${natLarge2} 1000w`}
           sizes="(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px"
           alt="Nat-2"
           className="composition__photo composition__photo--p2"
           src="img/nat-2-large.jpg" />
 
-        <img srcSet="img/nat-3.jpg 300w, img/nat-3-large.jpg 1000w"
+        <img srcSet={`${nat3} 300w, ${natLarge3} 1000w`}
           sizes="(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px"
           alt="Nat-3"
           className="composition__photo composition__photo--p3"
